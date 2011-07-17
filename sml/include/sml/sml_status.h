a64b4d58e611694f69bc0523ff71b43962071b07
Juri Glass <juri.glass@dai-labor.de>
2011-07-17 12:27:00 +0200

    added write, parse, init for sml_status

--- sml/include/sml/sml_status.h
+++ sml/include/sml/sml_status.h
@@ -20,22 +20,28 @@
 #define	_SML_STATUS_H
 
 #include "sml_number.h"
+#include "sml_shared.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 
-typedef union {
-	u8 *status8;
-	u16 *status16;
-	u32 *status32;
-	u64 *status64;
+typedef struct {
+	u8 type;
+	union {
+		u8 *status8;
+		u16 *status16;
+		u32 *status32;
+		u64 *status64;
+	} data;
 } sml_status;
 
+sml_status *sml_status_init();
 sml_status *sml_status_parse(sml_buffer *buf);
-
+void sml_status_write(sml_status *status, sml_buffer *buf);
 void sml_status_free(sml_status *status);
+
 #ifdef __cplusplus
 }
 #endif
--- sml/src/sml_status.c
+++ sml/src/sml_status.c
@@ -18,52 +18,58 @@
 
 
 #include <sml/sml_status.h>
-#include <sml/sml_shared.h>
+
+sml_status *sml_status_init() {
+	sml_status *status = (sml_status *) malloc(sizeof(sml_status));
+	memset(status, 0, sizeof(sml_status));
+	return status;
+}
 
 sml_status *sml_status_parse(sml_buffer *buf) {
+	if (sml_buf_optional_is_skipped(buf)) {
+		return 0;
+	}
+	
+	int max = 1;
 	int type = sml_buf_get_next_type(buf);
 	unsigned char byte = sml_buf_get_current_byte(buf);
 	
-	sml_status *state = (sml_status *) malloc(sizeof(sml_status));
+	sml_status *status = (sml_status *) malloc(sizeof(sml_status));
+	status->type = type;
 	switch (type) {
 		case SML_TYPE_UNSIGNED:
-			switch (byte & SML_LENGTH_FIELD) {
-				case SML_TYPE_NUMBER_8:
-					state->status8 = sml_u8_parse(buf);
-					break;
-				case SML_TYPE_NUMBER_16:
-					state->status16 = sml_u16_parse(buf);
-					break;
-				case SML_TYPE_NUMBER_16 + 1:
-				case SML_TYPE_NUMBER_32:
-					state->status32 = sml_u32_parse(buf);
-					break;
-				case SML_TYPE_NUMBER_32 + 1:
-				case SML_TYPE_NUMBER_32 + 2:
-				case SML_TYPE_NUMBER_32 + 3:
-				case SML_TYPE_NUMBER_64:
-					state->status64 = sml_u64_parse(buf);
-					break;
-				default:
-					buf->error = 1;
-					break;
+			// get maximal size, if not all bytes are used (example: only 6 bytes for a u64)
+			while (max < ((byte & SML_LENGTH_FIELD) - 1)) { 
+				max <<= 1;
 			}
+
+			status->data.status8 = sml_number_parse(buf, type, max);
+			status->type |= max;
 			break;
 		default:
 			buf->error = 1;
 			break;
 	}
 	if (sml_buf_has_errors(buf)) {
-		sml_status_free(state);
+		sml_status_free(status);
 		return 0;
 	}
 	
-	return state;
+	return status;
 }
 
+void sml_status_write(sml_status *status, sml_buffer *buf){
+	if (status == 0) {
+		sml_buf_optional_write(buf);
+		return;
+	}
+	sml_number_write(status->data.status8, (status->type & SML_TYPE_FIELD), 
+				(status->type & SML_LENGTH_FIELD), buf);
+};
+
 void sml_status_free(sml_status *status) {
-    if (status) {
-		sml_number_free(status->status8);
-        free(status);
-    }
+	if (status) {
+		sml_number_free(status->data.status8);
+		free(status);
+	}
 }
