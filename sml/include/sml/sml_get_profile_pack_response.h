4339767c4925d58644f7c9975013d31b145821c0
Juri Glass <juri.glass@dai-labor.de>
2011-09-03 16:29:29 +0200

    added sml_prof_obj_period_entry

--- sml/include/sml/sml_get_profile_pack_response.h
+++ sml/include/sml/sml_get_profile_pack_response.h
@@ -29,14 +29,13 @@
 extern "C" {
 #endif
 
-
 typedef struct {
 	octet_string *server_id;
 	sml_time *act_time; // specified by germans (current time was meant) ;)
 	u32 *reg_period;
 	sml_tree_path *parameter_tree_path;
-	sml_list *list_of_sml_prof_obj_header_entry;
-	sml_list *list_of_sml_prof_obj_period_entry;
+	sml_sequence *header_list;
+	sml_sequence *period_list;
 	octet_string *rawdata;  			// optional
 	sml_signature *profile_signature; 	// optional
 
@@ -46,6 +45,25 @@ sml_get_profile_pack_response *sml_get_profile_pack_response_init();
 sml_get_profile_pack_response *sml_get_profile_pack_response_parse(sml_buffer *buf);
 void sml_get_profile_pack_response_write(sml_get_profile_pack_response *msg, sml_buffer *buf);
 void sml_get_profile_pack_response_free(sml_get_profile_pack_response *msg);
+
+typedef struct {
+	
+	octet_string *obj_name;
+	sml_unit *unit;
+	i8 *scaler;
+} sml_prof_obj_header_entry;
+
+sml_prof_obj_header_entry *sml_prof_obj_header_entry_init();
+sml_prof_obj_header_entry *sml_prof_obj_header_entry_parse(sml_buffer *buf);
+void sml_prof_obj_header_entry_write(sml_prof_obj_header_entry *entry, sml_buffer *buf);
+void sml_prof_obj_header_entry_free(sml_prof_obj_header_entry *entry);
+
+typedef struct {
+	sml_time *val_time;
+	u64 *status;
+	sml_sequence *value_list;
+	sml_signature *period_signature;
+} sml_prof_obj_period_entry;
 
 #ifdef __cplusplus
 }
