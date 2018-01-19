93fafc65a5ff66ad4b7ec9970c6f3f692aebe0e3
Holger Müller <github@euhm.de>
2018-01-20 00:37:29 +0100

    Ignore when timestamp is a list (Frotec meter) (#37)

--- sml/src/sml_time.c
+++ sml/src/sml_time.c
@@ -51,8 +51,30 @@ sml_time *sml_time_parse(sml_buffer *buf) {
 	tme->tag = sml_u8_parse(buf);
 	if (sml_buf_has_errors(buf)) goto error;
 
-	tme->data.timestamp = sml_u32_parse(buf);
-	if (sml_buf_has_errors(buf)) goto error;
+	int type = sml_buf_get_next_type(buf);
+	switch (type) {
+	case SML_TYPE_UNSIGNED:
+		tme->data.timestamp = sml_u32_parse(buf);
+		if (sml_buf_has_errors(buf)) goto error;
+		break;
+	case SML_TYPE_LIST:
+		// Some meters (e.g. FROETEC Multiflex ZG22) giving not one uint32
+		// as timestamp, but a list of 3 values.
+		// Ignoring these values, so that parsing does not fail.
+		sml_buf_get_next_length(buf); // should we check the length here?
+		u32 *t1 = sml_u32_parse(buf);
+		if (sml_buf_has_errors(buf)) goto error;
+		i16 *t2 = sml_i16_parse(buf);
+		if (sml_buf_has_errors(buf)) goto error;
+		i16 *t3 = sml_i16_parse(buf);
+		if (sml_buf_has_errors(buf)) goto error;
+		fprintf(stderr,
+			"libsml: error: sml_time as list[3]: ignoring value[0]=%u value[1]=%d value[2]=%d\n",
+			*t1, *t2, *t3);
+		break;
+	default:
+		goto error;
+	}
 
 	return tme;
 
